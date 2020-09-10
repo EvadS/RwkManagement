97f88c16f57c119dc2afe0cdd7c2c06186cb5987
evgeniy_skiba <eugene.s@it-dimension.com>
2020-09-10 11:25:21 +0300

    initial commit

---
+++ src/main/java/com/se/management/RwManagementApplication.java
@@ -0,0 +1,13 @@
+package com.se.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RwManagementApplication.class, args);
+    }
+
+}
