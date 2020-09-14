62e41dfea1302f581c85af4d61ae7497e841f2d8
evgeniy_skiba <eugene.s@it-dimension.com>
2020-09-14 18:30:09 +0300

    changed:  fixed jpa auiting

--- src/main/java/com/se/management/RwManagementApplication.java
+++ src/main/java/com/se/management/RwManagementApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class RwManagementApplication  implements CommandLineRunner {
     @Autowired
